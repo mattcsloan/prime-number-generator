58daca08e864fe5d3d9ce96100ae063a0e088ecc
Matt Sloan <sloan@Matts-MacBook-Pro.local>
2017-09-06 00:29:26 -0400

    Added isPrime function to determine if a number is prime. isPrime test now passes

--- src/primeNumberGenerator.ts
+++ src/primeNumberGenerator.ts
@@ -4,5 +4,14 @@ export interface PrimeNumberGenerator {
 }
 
 export const isPrime = (currentNumber: number) => {
+  if(currentNumber === 2) {
+    return true;
+  } else if(currentNumber % 2 === 0 || currentNumber < 2) {
+    return false;
+  } else {
+    for (var n = 2; n <= Math.sqrt(currentNumber); n++) {
+      if (currentNumber % n === 0) return false;
+    }
+  }
   return true;
 };
